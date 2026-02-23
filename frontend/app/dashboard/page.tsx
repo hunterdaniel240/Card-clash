718877785925eb6d0bbe14be0e1fb700772924a6
hunterdaniel240 <h.daniel2401@gmail.com>
2026-02-23 16:33:27 -0600

    fix login structure and update pages

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-amber-950 font-sans dark:bg-black">
-      dashnoard
-    </div>
-  );
+export default function DashboardPage() {
+  return <div>Dashboard</div>
 }
---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function LoginPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1>Login Page</h1>
+    </div>
+  )
+}
